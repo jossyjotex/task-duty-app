78038e94439542db889f34c05baf2f9783d22fb0
josephine <jotex2014@gmail.com>
2025-05-16 13:58:11 +0100

    my project

--- src/Pages/NewTask.jsx
+++ src/Pages/NewTask.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Form from '../Components/Form'
+import { useState } from 'react'
 
 const NewTask = () => {
+  const [formData, setFormData] = useState({
+    taskTitle:"",
+    description:"",
+    tag:""
+  })
+
+  const handleChange =(e) =>{
+    const {name,value} = e.target;
+    setFormData((prev) =>({...prev,[name]:value}))
+
+  }
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+  }
   return (
     <>
-    <main>
-        <h1>NewTask</h1>
+    <main className='wrapper'>
+        <Form FormTitle="New Task" formData={formData} onChange={handleChange} onSubmit={handleSubmit}/>
     </main>
 
     </>
